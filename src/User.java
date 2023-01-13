e623aefe80d0aa005e12010c55c710bc56f3212d
Rakshith <rakshithravi5@gmail.com>
2023-01-14 02:26:06 +0530

    Creating Class

--- src/User.java
+++ src/User.java
@@ -1,11 +1,29 @@
-import javax.jws.soap.SOAPBinding;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class User {
     private String firstName;
     private String lastName;
     private String uuid;
-    private String pinHash[];
+    private byte pinHash[];
     private ArrayList<Account> accounts;
 
+
+    public User(String firstName, String lastName, String pin, Bank theBank){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            this.pinHash = md.digest(pin.getBytes());
+
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("error, caught NoSuchAlgorithmException");
+            System.exit(1);
+        }
+        this.uuid = theBank.getNewUserUUID();
+        this.accounts = new ArrayList<Account>();
+
+        System.out.printf("New user %s %s with ID %s created.\n", lastName,firstName,this.uuid);
+    }
 }
